202027c79d88dfea16d54c9abe1a34308b9e6dec
1010519rg46ft9o <slava.osadchyi.dev@gmail.com>
2019-04-27 18:03:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Last.fm App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
