9bbf8c582f0f10ba4e0fff2449bc116dc92e4d71
1010519rg46ft9o <slava.osadchyi.dev@gmail.com>
2019-05-01 19:10:10 +0300

    remove some files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import topTracks from './topTracks';
-import searchedTracks from './searchedTracks';
 import artist from './artist';
 
 const radioApp = combineReducers({
     topTracks,
-    searchedTracks,
     artist,
 })
 
